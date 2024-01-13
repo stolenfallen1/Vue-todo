088f4d5041fabfa42c73c491fe4ef638a1d59363
Paw <76894433+stolenfallen1@users.noreply.github.com>
2024-01-13 20:50:24 +0800

    feature: header name save to localstorage and todo forms

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,78 @@
-<script setup></script>
-
 <template>
-  <div>Hello World</div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Hello, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
+  </main>
 </template>
+
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+// ref: Creates a reactive reference to a value.
+// onMounted: Runs code after a component is added to the webpage.
+// computed: Calculates a value based on other values.
+// watch: Performs an action when a specific value changes.
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+const addTodo = () => {};
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
